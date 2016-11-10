2135277300de045486403404b78508ff2802d222
Christopher Brown <Chrisbrown0419@gmail.com>
2016-11-10 16:28:01 -0700

    First commit

---
+++ loop-practice-1.js
@@ -0,0 +1,36 @@
+// 0 - 100 - 200 - 300 - 400 - 500 
+for (var p = 0; p < 501; p+=100){
+    console.log(p);
+}
+
+//1 2 4 8 16 32 64
+for (var j = 1; j <65; j+=j){
+    console.log(j);
+}
+
+// 1 1 1 2 2 2 3 3 3
+for (var k = 1; k<4; k++){
+    console.log(k);
+       console.log(k);
+          console.log(k);
+}
+//0 2 4 6 8 10
+for (var e = 0; e<11; e+=2){
+    console.log(e);
+}
+
+//3 6 9 12 15
+for (var t = 3; t <16; t+=3){
+    console.log(t)
+}
+
+//9 8 7 6 5 4 3 2 1 0
+for (var y = 9; y>-1; y-=1){
+    console.log(y);
+}
+
+//0 1 2 3 0 1 2 3 0 1 2 3
+for(var z = 0; z<3; z++){
+ for(var j=0; j<4; j++){
+    console.log(j)}
+}
